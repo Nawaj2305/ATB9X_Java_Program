20870370dc0a028301550a1eabba842b73e8741d
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2024-10-23 08:36:47 +0530

    ALL the files

---
+++ src/Task/Task_03102024/Task2.java
@@ -0,0 +1,19 @@
+package Task.Task_03102024;
+
+public class Task2 {
+
+    //Program for 10 table
+    public static void main(String[] args){
+        System.out.println("10*1=10");
+        System.out.println("10*2=20");
+        System.out.println("10*3=30");
+        System.out.println("10*4=40");
+        System.out.println("10*5=50");
+        System.out.println("10*6=60");
+        System.out.println("10*7=70");
+        System.out.println("10*8=80");
+        System.out.println("10*9=90");
+        System.out.println("10*10=100");
+
+    }
+}
---
+++ src/Task/Task_03102024/Task3.java
@@ -0,0 +1,15 @@
+package Task.Task_03102024;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Task3 {
+    // simple calculator program
+
+    public static void main(String[] args) {
+        System.out.println(10+2);
+        System.out.println(10-2);
+        System.out.println(10*2);
+        System.out.println(10/2);
+    }
+}
+
