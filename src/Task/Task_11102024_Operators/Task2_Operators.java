4e7ae7fcf9783a7c2432434c4efdee0703782068
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2024-10-26 13:09:46 +0530

    ALL the files

---
+++ src/Task/Task_11102024_Operators/Task2_Operators.java
@@ -0,0 +1,48 @@
+package Task.Task_11102024_Operators;
+
+public class Task2_Operators {
+
+    public static void main(String[] args){
+
+        int a=40;
+        int b=20;
+
+        // Arithmatic Operators
+        System.out.println("1. ARITHMATIC OPERATORS");
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        //Relational Operators
+        System.out.println("2. RELATIONAL OPERATORS");
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+        //Logical Operators
+
+        System.out.println("3. LOGICAL OPERATORS");
+        System.out.println(a>b && b>a);
+        System.out.println(a>b || b>a);
+        System.out.println(a<b && b<a);
+        System.out.println(a<b || b<a);
+
+        //Assignment operators
+        System.out.println("4. Assignment OPERATORS");
+        System.out.println(a+=b);
+        System.out.println(a-=b) ;
+        System.out.println(a*=b) ;
+        System.out.println(a/=b) ;
+        System.out.println(a%=b) ;
+
+
+
+
+    }
+
+}
