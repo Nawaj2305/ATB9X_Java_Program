f1586eab901ab095a793188a8e9cb5d9fc0335d1
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2024-10-26 18:07:19 +0530

    ALL the files

---
+++ src/Task/Task_15102024/Task1_Ternary_Operator.java
@@ -0,0 +1,12 @@
+package Task.Task_15102024;
+
+public class Task1_Ternary_Operator {
+
+    public static void main(String[] args){
+
+        int a=10, b=20, c=30;
+
+        System.out.println((a>b && a>c)?a: (b>c)?b : "c is max");
+    }
+
+}
