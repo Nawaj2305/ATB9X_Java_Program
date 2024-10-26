8e9e7b5b27075bac29bc87cf1d49146cf40bc4d8
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2024-10-26 18:23:00 +0530

    ALL the files

---
+++ src/Task/Task_15102024/Task3_IncrDecre.java
@@ -0,0 +1,10 @@
+package Task.Task_15102024;
+
+public class Task3_IncrDecre {
+
+    public static void main(String[] args){
+        int a = 20;
+
+        System.out.println(--a + a++ + a--);
+    }
+}
