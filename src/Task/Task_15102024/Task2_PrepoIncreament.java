544b1a2e829eb53cd9f65cab790551dd233469f1
Nawaj.Shaikh <nawajshaikh23@gmail.com>
2024-10-26 18:13:03 +0530

    ALL the files

---
+++ src/Task/Task_15102024/Task2_PrepoIncreament.java
@@ -0,0 +1,13 @@
+package Task.Task_15102024;
+
+public class Task2_PrepoIncreament {
+
+    public static void main(String[] args){
+
+        int a = 10;
+
+        System.out.println(++a + a++ + a++);
+
+        System.out.println(a);
+    }
+}
